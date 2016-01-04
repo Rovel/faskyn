dc468429f5648a3e3572e94b44a167fd6605e5fa
Filipe Veloso <filipe.roman.veloso@gmail.com>
2016-01-04 21:56:12 -0200

    refile fix

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,6 +4,7 @@ if Rails.env.production?
   aws = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: "us-east-1",
     bucket: ENV['AWS_BUCKET_NAME']
   }
   Refile.cache = Refile::S3.new(max_size: 5.megabytes, prefix: "cache", **aws)
